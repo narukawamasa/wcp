b4fb00004a0771d844d00db56157c0cf19561373
narukawamasa <te2.icpo19@gmail.com>
2023-02-13 14:40:34 +0900

    変数確認問題

--- Chapter04.java
+++ Chapter04.java
@@ -1,10 +1,10 @@
 public class Chapter04 {
     public static void main(String[] args) {
-        string str1;
+        String str1;
         str1 = "令和太郎";
         System.out.println(str1);
 
-        str1 = "やればできる！"
+        str1 = "やればできる！";
         System.out.println(str1);
 
         int num1;
