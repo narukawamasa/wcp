dbf07be7a79f95f6ca40eab906a4e7d5412525a6
narukawamasa <te2.icpo19@gmail.com>
2023-02-13 14:36:52 +0900

    test5

---
+++ Chapter04.java
@@ -0,0 +1,14 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        string str1;
+        str1 = "令和太郎";
+        System.out.println(str1);
+
+        str1 = "やればできる！"
+        System.out.println(str1);
+
+        int num1;
+        num1 = 777;
+        System.out.println(num1);
+    }
+}
