97d9c5ef99facac2e00f6ad2cdf97acc085a3ef4
narukawamasa <te2.icpo19@gmail.com>
2023-02-14 14:27:29 +0900

    七章確認問題１

---
+++ Chapter07.java
@@ -0,0 +1,14 @@
+public class Chapter07 {
+    public static void main(String[] args) {
+        int val = 8;
+        if (val <= 5) {
+            System.out.println("とても近いです");
+        } else if (val <= 10) {
+            System.out.println("近いです");
+        } else if (val <=15) {
+            System.out.println("遠いです");
+        } else {
+            System.out.println("とても遠いです");
+        }
+    }
+}
