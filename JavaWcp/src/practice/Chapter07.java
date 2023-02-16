6b532818bfed997c8d8538c86b91960bc5fc22da
narukawamasa <te2.icpo19@gmail.com>
2023-02-16 13:36:01 +0900

    ７章確認問題１、二回目

---
+++ JavaWcp/src/practice/Chapter07.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		int num = 8;
+		if (num <= 5) {
+			System.out.println("とても近いです");
+		} else if (num <= 10) {
+			System.out.println("近いです");
+		} else if (num <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+	}
+
+}
