5a01c9f043b989f4b5d311d1fd2c9124602b29d4
narukawamasa <te2.icpo19@gmail.com>
2023-02-17 19:50:52 +0900

    9章サンプル６

---
+++ JavaWcp/src/classMethod/ClassMethod05_1.java
@@ -0,0 +1,12 @@
+package classMethod;
+
+import classMethod.human.Human05_1;
+
+public class ClassMethod05_1 {
+	public static void main(String[] args) {
+		Human05_1 yamada = new Human05_1();
+		
+		System.out.println("名前は" + yamada.getName() + "で、年齢は" + yamada.getAge() + "です。");
+	}
+
+}
