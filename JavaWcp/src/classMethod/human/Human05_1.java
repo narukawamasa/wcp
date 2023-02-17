703129220a40a2d2e3d964eda1b6543370eb58dc
narukawamasa <te2.icpo19@gmail.com>
2023-02-17 19:51:03 +0900

    ９章サンプル６

---
+++ JavaWcp/src/classMethod/human/Human05_1.java
@@ -0,0 +1,21 @@
+package classMethod.human;
+
+public class Human05_1 {
+	private String name;
+	private int age;
+	
+	public Human05_1() {
+		name = "山田";
+		age = 20;
+	}
+	
+	public String getName() {
+		return name;
+		
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+}
