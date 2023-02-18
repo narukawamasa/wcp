0cb0de68a513f324745057ebf48f63b6cafcfa49
narukawamasa <te2.icpo19@gmail.com>
2023-02-18 13:20:46 +0900

    10章サンプル１

---
+++ JavaWcp/src/exception/Exception01_1.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class Exception01_1 {
+	public static void main(String[] args) {
+		System.out.println("100 ÷ 0 は？");
+		int result = 100 / 0;
+		System.out.println("計算結果" + result);
+		System.out.println("プログラミング終了");
+	}
+
+}
