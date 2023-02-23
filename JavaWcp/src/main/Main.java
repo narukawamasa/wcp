dd9d30ccf004674a087dc76f15c46c975a459f7b
narukawamasa <te2.icpo19@gmail.com>
2023-02-23 19:01:29 +0900

    スッキリjava確認問題

--- JavaWcp/src/main/Main.java
+++ JavaWcp/src/main/Main.java
@@ -2,11 +2,10 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		int age = 20;
-		System.out.println("私の年齢は" + age);
-		
-		age = 31;
-		System.out.println("…いや、本当の年齢は" + age);
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅" + a + "横幅" + b +"の長方形の面積は、" + c);
 	}
 
 }
