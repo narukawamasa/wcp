82330bbc7d21f3c35bf65db6ceb6b939f638be80
narukawamasa <te2.icpo19@gmail.com>
2023-02-24 11:40:27 +0900

    一部練習問題3-3

---
+++ JavaWcp/src/main/J_1_r_3_3.java
@@ -0,0 +1,18 @@
+package main;
+
+public class J_1_r_3_3 {
+	public static void main(String[] args) {
+		int isHungry = 0;
+		String food = "いちご";
+		System.out.println("こんにちは");
+		
+		if (isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			System.out.println("はらぺこです");
+			System.out.println(food + "をいただきます");
+		}
+		System.out.println("ごちそうさまでした");
+	}
+
+}
