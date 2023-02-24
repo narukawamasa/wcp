08939724cbb53e745a609b8d922d311189ded32b
narukawamasa <te2.icpo19@gmail.com>
2023-02-24 17:34:22 +0900

    一部練習問題5-1

---
+++ JavaWcp/src/main/J_1_r_5_1.java
@@ -0,0 +1,17 @@
+package main;
+
+public class J_1_r_5_1 {
+	
+	public static void introduceOneself() {
+		String name = "田中";
+		int age = 20;
+		float tall = 180;
+		char an = '丑';
+		System.out.println(name + "です。年齢" + age + "、身長は" + tall + "、干支は" + an + "です。");
+	}
+	
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+
+}
