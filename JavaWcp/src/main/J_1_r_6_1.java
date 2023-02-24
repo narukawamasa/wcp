54953a0f3c139d27977661818b87208e67dc0564
narukawamasa <te2.icpo19@gmail.com>
2023-02-24 19:20:59 +0900

    一部確認問題6

---
+++ JavaWcp/src/main/J_1_r_6_1.java
@@ -0,0 +1,13 @@
+package main;
+
+import main.comment.Zenhan;
+
+public class J_1_r_6_1 {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		main.comment.Kouhan.callDeae();
+		main.comment.Kouhan.showMondokoro();
+	}
+
+}
