df46329a98b6023b7b73e001c616e5e110953f46
narukawamasa <te2.icpo19@gmail.com>
2023-02-26 12:30:57 +0900

    二部練習問題8-3

--- JavaWcp/src/main/game/Cleric.java
+++ JavaWcp/src/main/game/Cleric.java
@@ -6,5 +6,12 @@ public class Cleric {
 	final int max_Hp = 50;
 	int mp = 10;
 	final int max_Mp = 10;
+	
+	public void selfAid() {
+		System.out.println(this.name + "は、セルフエイドを唱えた！");
+		this.mp -= 5;
+		this.hp = this.max_Hp;
+		System.out.println("HPが最大まで回復した！");
+	}
 
 }
