bb1629d15e11b25d3cbceb79ea54be77d1b24881
narukawamasa <te2.icpo19@gmail.com>
2023-02-26 12:40:51 +0900

    二部練習問題8-4

--- JavaWcp/src/main/game/Cleric.java
+++ JavaWcp/src/main/game/Cleric.java
@@ -1,5 +1,7 @@
 package main.game;
 
+import java.util.Random;
+
 public class Cleric {
 	String name;
 	int hp = 50;
@@ -12,6 +14,18 @@ public class Cleric {
 		this.mp -= 5;
 		this.hp = this.max_Hp;
 		System.out.println("HPが最大まで回復した！");
+	}
+	
+	public int pray(int sec) {
+		System.out.println(this.name + "は" + sec + "秒間祈った！");
+		
+		int recover = new Random().nextInt(3) + sec;
+		
+		int recoverActual = Math.min(this.max_Mp - this.mp, recover);
+		
+		this.mp += recoverActual;
+		System.out.println("MPが" + recoverActual + "回復した");
+		return recoverActual;
 	}
 
 }
