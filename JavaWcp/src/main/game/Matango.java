6ea72be5df9761028534a99aa906b9c91c2bf4d7
narukawamasa <te2.icpo19@gmail.com>
2023-02-26 17:57:13 +0900

    二部練習問題10-2

--- JavaWcp/src/main/game/Matango.java
+++ JavaWcp/src/main/game/Matango.java
@@ -4,6 +4,11 @@ public class Matango {
 	int hp;
 	final int LEVEL = 10;
 	char suffix;
+	
+	public Matango(char suffix) {
+		this.suffix = suffix;
+	}
+	
 	public void run() {
 		System.out.println(
 				"お化けキノコ" + this.suffix + "は逃げ出した！");
