ebe8c98d636e29281587e6e1b7a293b2f1f80517
narukawamasa <te2.icpo19@gmail.com>
2023-02-27 10:49:40 +0900

    二部練習問題11-1

--- JavaWcp/src/main/Book.java
+++ JavaWcp/src/main/Book.java
@@ -1,28 +1,12 @@
 package main;
 
-public class Book {
-	String name;
-	int price;
-	String color;
+public class Book extends TangibleAsset {
+	
 	String isbn;
 	
 	public Book(String name, int price, String color, String isbn) {
-		this.name = name;
-		this.price = price;
-		this.color = color;
+		super(name,price,color);
 		this.isbn = isbn;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public int getPrice() {
-		return this.price;
-	}
-	
-	public String getColor() {
-		return this.color;
 	}
 	
 	public String getIsbn() {
--- JavaWcp/src/main/Computer.java
+++ JavaWcp/src/main/Computer.java
@@ -1,28 +1,12 @@
 package main;
 
-public class Computer {
-	String name;
-	int price;
-	String color;
+public class Computer extends TangibleAsset {
+
 	String makerName;
 	
 	public Computer(String name, int price, String color, String makerName) {
-		this.name = name;
-		this.price = price;
-		this.color = color;
+		super(name,price,color);
 		this.makerName = makerName;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public int getPrice() {
-		return this.price;
-	}
-	
-	public String getColor() {
-		return this.color;
 	}
 	
 	public String getMakerName() {
