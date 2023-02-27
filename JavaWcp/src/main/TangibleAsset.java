9431ca2ec79bf1b54ecc08fe57c8ddec698a37cb
narukawamasa <te2.icpo19@gmail.com>
2023-02-27 11:14:36 +0900

    二部練習問題11-4

--- JavaWcp/src/main/TangibleAsset.java
+++ JavaWcp/src/main/TangibleAsset.java
@@ -1,7 +1,8 @@
 package main;
 
-public abstract class TangibleAsset extends Asset {
+public abstract class TangibleAsset extends Asset implements Thing {
 	String color;
+	double weight;
 	
 	public TangibleAsset(String name, int price, String color) {
 		super(name,price);
@@ -10,6 +11,14 @@ public abstract class TangibleAsset extends Asset {
 	
 	public String getColor() {
 		return this.color;
+	}
+	
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	public double getWeight() {
+		return this.weight;
 	}
 
 }
