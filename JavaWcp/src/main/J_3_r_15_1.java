28ac8cd3632a1d796d8562948dae15b0ead3d694
narukawamasa <te2.icpo19@gmail.com>
2023-02-27 17:51:59 +0900

    三部練習問題15-1

---
+++ JavaWcp/src/main/J_3_r_15_1.java
@@ -0,0 +1,17 @@
+package main;
+
+public class J_3_r_15_1 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 100; i++) {
+			sb.append(i+1).append(",");
+		}
+		String s = sb.toString();
+		String[] a = s.split(",");
+		
+	}
+	
+	
+	
+
+}
