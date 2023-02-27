108cab6256190ca2b3bd0dce76f84f27aa7ab21e
narukawamasa <te2.icpo19@gmail.com>
2023-02-27 10:38:31 +0900

    二部練習問題11-1準備

--- JavaWcp/src/main/Computer.java
+++ JavaWcp/src/main/Computer.java
@@ -1,5 +1,32 @@
 package main;
 
 public class Computer {
+	String name;
+	int price;
+	String color;
+	String makerName;
+	
+	public Computer(String name, int price, String color, String makerName) {
+		this.name = name;
+		this.price = price;
+		this.color = color;
+		this.makerName = makerName;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	public String getMakerName() {
+		return this.makerName;
+	}
 
 }
