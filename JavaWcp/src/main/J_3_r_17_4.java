1c96296cebf9a881d9663a4ee7c22050851ad87b
narukawamasa <te2.icpo19@gmail.com>
2023-02-28 17:06:51 +0900

    三部練習問題17-5

---
+++ JavaWcp/src/main/J_3_r_17_4.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.io.IOException;
+
+public class J_3_r_17_4 {
+	public static void main(String[] args) throws IOException {
+		System.out.println("プログラムが起動しました");
+		throw new IOException();
+	}
+
+}
