9d79ae72d41dc17ac9a382ded9aa3f1bd45c3c7f
narukawamasa <te2.icpo19@gmail.com>
2023-02-28 17:01:29 +0900

    三部練習問題17-2

--- JavaWcp/src/main/J_3_r_17_1_2.java
+++ JavaWcp/src/main/J_3_r_17_1_2.java
@@ -2,8 +2,15 @@ package main;
 
 public class J_3_r_17_1_2 {
 	public static void main(String[] args) {
-		String s = null;
-		System.out.println(s.length());
+		try {
+			String s = null;
+			System.out.println(s.length());
+		} catch (NullPointerException e) {
+			System.out.println("NullPointerException例外をcatchしました");
+			System.out.println("--スタックトレース(ここから)--");
+			e.printStackTrace();
+			System.out.println("--スタックトレース(ここまで)--");
+		}
 	}
 
 }
