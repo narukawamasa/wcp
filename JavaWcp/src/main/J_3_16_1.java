20b13e0f32329072278cb9af6797cc2f616d29cc
narukawamasa <te2.icpo19@gmail.com>
2023-02-28 12:16:52 +0900

    三部カリキュラム16-1

---
+++ JavaWcp/src/main/J_3_16_1.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.ArrayList;
+
+public class J_3_16_1 {
+	public static void main(String[] args) {
+		ArrayList<Integer> points = new ArrayList<Integer>();
+		points.add(10);
+		points.add(80);
+		points.add(75);
+		for (int i : points) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
