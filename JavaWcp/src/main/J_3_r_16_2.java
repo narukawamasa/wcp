823696c0613625b5dbc92cf72905784a67f86b83
narukawamasa <te2.icpo19@gmail.com>
2023-02-28 13:59:47 +0900

    三部練習問題16-3

--- JavaWcp/src/main/J_3_r_16_2.java
+++ JavaWcp/src/main/J_3_r_16_2.java
@@ -1,20 +1,21 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class J_3_r_16_2 {
 	public static void main(String[] args) {
 		Hero_2 h1 = new Hero_2("斎藤");
 		Hero_2 h2 = new Hero_2("鈴木");
 		
-		List<Hero_2> heros = new ArrayList<Hero_2>();
+		Map<Hero_2, Integer> heroes = new HashMap<Hero_2, Integer>();
 		
-		heros.add(h1);
-		heros.add(h2);
+		heroes.put(h1, 3);
+		heroes.put(h2, 7);
 		
-		for (Hero_2 s : heros) {
-			System.out.println(s.getName());
+		for (Hero_2 key : heroes.keySet()) {
+			Integer value = heroes.get(key);
+			System.out.println(key.getName() + "が倒した敵＝" + value);
 		}
 	}
 
