1fe0900a82d42673196efbfbf44a8272f786d5ab
narukawamasa <te2.icpo19@gmail.com>
2023-03-01 18:31:17 +0900

    一部カリキュラム2-6

--- JavaWcp/src/main_2/J_1_2_3.java
+++ JavaWcp/src/main_2/J_1_2_3.java
@@ -2,9 +2,9 @@ package main_2;
 
 public class J_1_2_3 {
 	public static void main(String[] args) {
-		Pocket p = new Pocket();
+		Pocket<String> p = new Pocket<>();
 		p.put("1192");
-		String s = (String)p.get();
+		String s = p.get();
 		System.out.println(s);
 	}
 
