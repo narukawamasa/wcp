98e2589ca781843a41d0343ed6fc289320c91189
narukawamasa <te2.icpo19@gmail.com>
2023-03-02 18:06:06 +0900

    一部カリキュラム4-1

---
+++ JavaWcp/src/main_2/J_1_4_1.java
@@ -0,0 +1,13 @@
+package main_2;
+
+public class J_1_4_1 {
+	public static void main(String[] args) {
+		boolean isErr = true;
+		if (isErr) {
+			System.out.println("データが壊れています。異常終了します");
+			System.exit(1);
+		}
+		System.out.println("正常終了しました");
+	}
+
+}
