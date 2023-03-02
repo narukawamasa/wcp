4b722f2d48ec7e6f7e29d4e5259e51d5c8074c8f
narukawamasa <te2.icpo19@gmail.com>
2023-03-02 17:27:09 +0900

    一部練習問題3-2

--- JavaWcp/src/main_2/j_3_r/FuncList.java
+++ JavaWcp/src/main_2/j_3_r/FuncList.java
@@ -2,7 +2,7 @@ package main_2.j_3_r;
 
 public class FuncList {
 	public static boolean isOdd(int x) {
-		return (x % 2 == 0);
+		return (x % 2 == 1);
 	}
 	
 	public String passCheck(int point, String name) {
--- JavaWcp/src/main_2/j_3_r/Main.java
+++ JavaWcp/src/main_2/j_3_r/Main.java
@@ -2,9 +2,10 @@ package main_2.j_3_r;
 
 public class Main {
 	public static void main(String[] args) {
-		FuncList funclist = new FuncList();
-		Func1 f1 = FuncList::isOdd;
-		Func2 f2 = funclist::passCheck;
+		Func1 f1 = x -> x % 2 == 1;
+		Func2 f2 = (point, name) -> {
+			return name + "さんは" + (point > 65 ? "合格" : "不合格");
+		};
 		System.out.println(f1.call(15));
 		System.out.println(f2.call(66, "Smith"));
 	}
