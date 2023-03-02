349b825f6992f352e6823c71dcbe6f0eedac1a79
narukawamasa <te2.icpo19@gmail.com>
2023-03-02 16:57:41 +0900

    一部カリキュラム3-6

---
+++ JavaWcp/src/main_2/J_1_3_6.java
@@ -0,0 +1,19 @@
+package main_2;
+
+import java.util.*;
+
+public class J_1_3_6 {
+	public static void main(String[] args) {
+		List<Hero> heroes = new ArrayList<>();
+		
+		boolean anyoneKnockedOut = false;
+		
+		for (Hero h : heroes) {
+			if (h,hp == 0) {
+				anyoneKnockedOut = true;
+				break;
+			}
+		}
+	}
+
+}
