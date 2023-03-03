c434adc252c64cd8866448d73fbd8a98235be5bc
narukawamasa <te2.icpo19@gmail.com>
2023-03-03 14:11:34 +0900

    一部カリキュラム5-3

--- JavaWcp/src/main_2/Bank.java
+++ JavaWcp/src/main_2/Bank.java
@@ -1,17 +1,14 @@
 package main_2;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 public class Bank {
 	String name;
 	String address;
 	
 	public boolean equals(Object o) {
-		if (o == this) return true;
-		if (o == null) return false;
-		if (!(o instanceof Bank)) return false;
-		Bank r = (Bank)o;
-		if (!this.name.equals(r.name)) return false;
-		if (!this.address.equals(r.address)) return false;
-		return true;
+		return EqualsBuilder.reflectionEquals(this, o);
+		
 	}
 
 }
