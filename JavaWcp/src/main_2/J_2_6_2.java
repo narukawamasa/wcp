11671fd9ddd5a7a1b409f0c7a046d709ba39ae83
narukawamasa <te2.icpo19@gmail.com>
2023-03-03 15:51:45 +0900

    一部カリキュラム6-2

--- JavaWcp/src/main_2/J_2_6_2.java
+++ JavaWcp/src/main_2/J_2_6_2.java
@@ -1,5 +1,21 @@
 package main_2;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class J_2_6_2 {
+	public static void main(String[] args) throws IOException {
+		FileReader fw = new FileReader("rpqsave.dat");
+		System.out.println("すべてのデータを読んで表示します");
+		
+		int i = fw.read();
+		while (i != -1) {
+			char c = (char)i;
+			System.out.println(c);
+			i = fw.read();
+		}
+		System.out.println("ファイルの末尾に到達しました");
+		fw.close();
+	}
 
 }
