bb1eb0c95b99b8e4b0e68c7d7d619b11b101edb2
narukawamasa <te2.icpo19@gmail.com>
2023-03-03 18:08:09 +0900

    二部カリキュラム7-4

---
+++ JavaWcp/src/main_2/J_2_7_4.java
@@ -0,0 +1,16 @@
+package main_2;
+
+public class J_2_7_4 {
+	public static void main(String[] args) throws Exception {
+//		FileReader fr = new FileReader("rpgdata.csv");
+//		Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(fr);
+//		for (CSVRecord r : records) {
+//			String name = r.get();
+//			String hp = r.get(1);
+//			String mp = r.get(2);
+//			System.out.println(name + "/" + hp + "/" + mp);
+//		}
+//		fr.close();
+	}
+
+}
