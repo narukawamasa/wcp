041905e3406136a6f45135ced05d9bbc19516d63
narukawamasa <te2.icpo19@gmail.com>
2023-03-03 17:58:18 +0900

    二部カリキュラム7-2

---
+++ JavaWcp/src/main_2/J_2_7_2.java
@@ -0,0 +1,21 @@
+package main_2;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class J_2_7_2 {
+	public void saveHeroToFile(Hero h) throws IOException {
+		Writer w = new BufferedWriter(new FileWriter("rpgsave.dat"));
+		w.write(h.name);
+		w.write(",");
+		w.write(h.hp);
+		w.write(",");
+//		w.wrire(h.mp);
+//		w.write(",");
+		w.flush();
+		w.close();
+	}
+
+}
