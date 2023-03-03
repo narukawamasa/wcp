478cdeff9c0c4251d12459eb6201a3870f109739
narukawamasa <te2.icpo19@gmail.com>
2023-03-03 18:01:35 +0900

    二部カリキュラム7-3

---
+++ JavaWcp/src/main_2/J_2_7_3.java
@@ -0,0 +1,13 @@
+package main_2;
+
+public class J_2_7_3 {
+	public static void main(String[] args) {
+		String s = "ミナト,アサカ,スガワラ";
+		String[] st = s.split(",");
+		
+		for (String t : st) {
+			System.out.println(t);
+		}
+	}
+
+}
