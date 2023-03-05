d3f999fd146e90900a82a68009afacd02ecbdf0e
narukawamasa <te2.icpo19@gmail.com>
2023-03-05 15:23:38 +0900

    二部練習問題7-3

--- JavaWcp/src/main_2/j_7_r/Main.java
+++ JavaWcp/src/main_2/j_7_r/Main.java
@@ -1,12 +1,23 @@
 package main_2.j_7_r;
 
-import java.util.ResourceBundle;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		ResourceBundle rb = ResourceBundle.getBundle("pref");
-		System.out.println(rb.getString("aichi.capital") + ":"
-				+ rb.getString("aichi.food"));
+		Employee tanaka = new Employee();
+		tanaka.name = "田中一郎";
+		tanaka.age = 41;
+		Department soumubu = new Department();
+		soumubu.name = "総務部";
+		soumubu.leader = tanaka;
+		FileOutputStream fos = new FileOutputStream("company.dat");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(soumubu);
+		oos.flush();
+		oos.close();
+		
+		
 	}
 
 }
