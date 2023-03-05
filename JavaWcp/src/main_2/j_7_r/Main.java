f010cc4a090f7b98d7ec251966cba1da7b1dc63d
narukawamasa <te2.icpo19@gmail.com>
2023-03-05 15:17:24 +0900

    二部練習問題7-2

--- JavaWcp/src/main_2/j_7_r/Main.java
+++ JavaWcp/src/main_2/j_7_r/Main.java
@@ -1,17 +1,12 @@
 package main_2.j_7_r;
 
-import java.io.FileReader;
-import java.io.Reader;
-import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Reader fr = new FileReader("pref.properties");
-		Properties p = new Properties();
-		p.load(fr);
-		System.out.println(p.getProperty("aichi.capital") + ":"
-				+ p.getProperty("aichi.food"));
-		fr.close();
+		ResourceBundle rb = ResourceBundle.getBundle("pref");
+		System.out.println(rb.getString("aichi.capital") + ":"
+				+ rb.getString("aichi.food"));
 	}
 
 }
