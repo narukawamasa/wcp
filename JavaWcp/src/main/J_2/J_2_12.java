05044651751970375db22b20585f01cc0486d9a7
narukawamasa <te2.icpo19@gmail.com>
2023-03-06 16:19:22 +0900

    二回目2-12

---
+++ JavaWcp/src/main/J_2/J_2_12.java
@@ -0,0 +1,11 @@
+package main.J_2;
+
+public class J_2_12 {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		int m = Math.max(a, b);
+		System.out.println("比較実験:" + a + "と" + b + "とで大きい方は" + m);
+	}
+
+}
