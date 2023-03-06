97a18c11b742046b16415a54bccfb0fa02a40796
narukawamasa <te2.icpo19@gmail.com>
2023-03-06 16:26:21 +0900

    二回目2-14

---
+++ JavaWcp/src/main/J_2/J_2_14.java
@@ -0,0 +1,9 @@
+package main.J_2;
+
+public class J_2_14 {
+	public static void main(String[] args) {
+		int r = new java.util.Random().nextInt(90);
+		System.out.println("あなたはたぶん、" + r + "歳ですね？");
+	}
+
+}
