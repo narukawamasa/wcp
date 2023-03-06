246d6135aab0db774f8f564baa52200cb214764e
narukawamasa <te2.icpo19@gmail.com>
2023-03-06 14:50:49 +0900

    二回目1-3

--- JavaWcp/src/main/j_1/J_1_3.java
+++ JavaWcp/src/main/j_1/J_1_3.java
@@ -3,8 +3,10 @@ package main.j_1;
 public class J_1_3 {
 	public static void main(String[] aegs) {
 		int age;
-		age = 30;
-		System.out.println(age);
+		age = 20;
+		System.out.println("私の年齢は" + age);
+		age = 31;
+		System.out.println("…いや、本当は" + age);
 	}
 
 }
