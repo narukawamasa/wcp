9a34512b4a7487511e3ed1248d31897af389ce9e
narukawamasa <te2.icpo19@gmail.com>
2023-03-06 16:56:18 +0900

    二回目3-1

---
+++ JavaWcp/src/main/J_3/J_3_1.java
@@ -0,0 +1,15 @@
+package main.J_3;
+
+public class J_3_1 {
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯します");
+			System.out.println("散歩にいきます");
+			
+		} else {
+			System.out.println("DVDを見ます");
+		}
+	}
+
+}
