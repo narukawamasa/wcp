ef48aed78e313dc9096f7576336484ec9f07e4d3
narukawamasa <te2.icpo19@gmail.com>
2023-03-06 16:09:12 +0900

    二回目2-8

---
+++ JavaWcp/src/main/J_2/J_2_8.java
@@ -0,0 +1,11 @@
+package main.J_2;
+
+public class J_2_8 {
+	public static void main(String[] aegs) {
+		double d = 8.5 / 2;
+		long l = 5 + 2L;
+		System.out.println(d);
+		System.out.println(l);
+	}
+
+}
