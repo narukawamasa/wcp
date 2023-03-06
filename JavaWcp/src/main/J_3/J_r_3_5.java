e31ee76e070ee4daf948ea72a6f57e748644310f
narukawamasa <te2.icpo19@gmail.com>
2023-03-06 17:44:56 +0900

    二回目r-3-5

---
+++ JavaWcp/src/main/J_3/J_r_3_5.java
@@ -0,0 +1,24 @@
+package main.J_3;
+
+public class J_r_3_5 {
+	public static void main(String[] args) {
+		System.out.print("[メニュー]1:検索2:登録3:削除4:変更>");
+		int selected = new java.util.Scanner(System.in).nextInt();
+		
+		switch (selected) {
+		case 1:
+			System.out.println("検索します");
+			break;
+		case 2:
+			System.out.println("登録します");
+			break;
+		case 3:
+			System.out.println("削除します");
+			break;
+		case 4:
+			System.out.println("変更します");
+			break;
+		}
+	}
+
+}
