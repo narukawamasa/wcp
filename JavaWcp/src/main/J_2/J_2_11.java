a29c823b8ade101604f859f0b224dd91cb32eb9d
narukawamasa <te2.icpo19@gmail.com>
2023-03-06 16:16:45 +0900

    二回目2-11

---
+++ JavaWcp/src/main/J_2/J_2_11.java
@@ -0,0 +1,11 @@
+package main.J_2;
+
+public class J_2_11 {
+	public static void main(String[] args) {
+		String name = "スガワラ";
+		System.out.print("私の名前は");
+		System.out.print(name);
+		System.out.print("です");
+	}
+
+}
