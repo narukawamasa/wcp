bf2ddfecc6e5d25cfc1a9a904ad23bfc3c171ab4
narukawamasa <te2.icpo19@gmail.com>
2023-03-06 17:38:34 +0900

    二回目r-3-3

---
+++ JavaWcp/src/main/J_3/J_r_3_3.java
@@ -0,0 +1,19 @@
+package main.J_3;
+
+public class J_r_3_3 {
+	public static void main(String[] args) {
+		int isHungry = 1;
+		String food = "いちご";
+		System.out.println("こんにちは");
+		if (isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			System.out.println("はらぺこです");
+		}
+		if (isHungry == 1) {
+			System.out.println(food + "をいただきます");
+		}
+		System.out.println("ごちそうさまでした");
+	}
+
+}
