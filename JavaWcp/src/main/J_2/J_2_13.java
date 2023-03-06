877f61fdf6479d8e8e1b7ee63a128de8ad47858d
narukawamasa <te2.icpo19@gmail.com>
2023-03-06 16:23:34 +0900

    二回目2-13

---
+++ JavaWcp/src/main/J_2/J_2_13.java
@@ -0,0 +1,10 @@
+package main.J_2;
+
+public class J_2_13 {
+	public static void main(String[] aegs) {
+		String age = "31";
+		int n = Integer.parseInt(age);
+		System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+	}
+
+}
