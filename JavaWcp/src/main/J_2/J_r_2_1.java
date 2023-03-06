469251f62476bd6b7259a98d611d40e4192bf835
narukawamasa <te2.icpo19@gmail.com>
2023-03-06 16:33:36 +0900

    二回目r-2-1

---
+++ JavaWcp/src/main/J_2/J_r_2_1.java
@@ -0,0 +1,11 @@
+package main.J_2;
+
+public class J_r_2_1 {
+	public static void main(String[] aegs) {
+		int x = 5;
+		int y = 10;
+		String ans = "x+yは" + (x + y);
+		System.out.println(ans);
+	}
+
+}
