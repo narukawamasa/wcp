439375f4dde37b0c290d4fe69e87246b2cb88f2d
narukawamasa <te2.icpo19@gmail.com>
2023-03-07 17:50:17 +0900

    二回目4-7

--- JavaWcp/src/main/J_4/J_4_6.java
+++
@@ -1,9 +0,0 @@
-package main.J_4;
-
-public class J_4_6 {
-	public static void main(String[] args) {
-		int x;
-		System.out.println(x);
-	}
-
-}
---
+++ JavaWcp/src/main/J_4/J_4_7.java
@@ -0,0 +1,9 @@
+package main.J_4;
+
+public class J_4_7 {
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+		System.out.println(scores[0]);
+	}
+
+}
