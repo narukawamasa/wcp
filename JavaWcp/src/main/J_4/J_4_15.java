c53bade8ac717268092151f39e845361547e0521
narukawamasa <te2.icpo19@gmail.com>
2023-03-07 18:31:51 +0900

    二回目4-15

---
+++ JavaWcp/src/main/J_4/J_4_15.java
@@ -0,0 +1,12 @@
+package main.J_4;
+
+public class J_4_15 {
+	public static void main(String[] args) {
+		int[] arrayA = {1,2,3};
+		int[] arrayB;
+		arrayB = arrayA;
+		arrayB[0] = 100;
+		System.out.println(arrayA[0]);
+	}
+
+}
