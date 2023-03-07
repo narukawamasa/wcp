ee65fbb545a42b3ea5f5a396b595ca5cf4ef8519
narukawamasa <te2.icpo19@gmail.com>
2023-03-07 17:44:30 +0900

    二回目4-4

---
+++ JavaWcp/src/main/J_4/j_4_4.java
@@ -0,0 +1,10 @@
+package main.J_4;
+
+public class j_4_4 {
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+		int num = scores.length;
+		System.out.println("要素の数:" + num);
+	}
+
+}
