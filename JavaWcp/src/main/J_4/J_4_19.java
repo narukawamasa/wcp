1bd5d5f8f40e941b30d899bb1d1db1cbb101c2bd
narukawamasa <te2.icpo19@gmail.com>
2023-03-07 18:44:01 +0900

    二回目4-19

---
+++ JavaWcp/src/main/J_4/J_4_19.java
@@ -0,0 +1,10 @@
+package main.J_4;
+
+public class J_4_19 {
+	public static void main(String[] args) {
+		int[][] scores = {{40,50,60},{80,60,70}};
+		System.out.println(scores.length);
+		System.out.println(scores[0].length);
+	}
+
+}
