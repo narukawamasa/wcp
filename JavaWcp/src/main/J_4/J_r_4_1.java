baeaa55d38bd58ca5cf670febabe294371d42d21
narukawamasa <te2.icpo19@gmail.com>
2023-03-07 18:48:19 +0900

    二回目r-4-1

---
+++ JavaWcp/src/main/J_4/J_r_4_1.java
@@ -0,0 +1,11 @@
+package main.J_4;
+
+public class J_r_4_1 {
+	public static void main(String[] args) {
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
+	}
+
+}
