822c8a6b69f8a8df40ec93f48f2e8d164d56fe97
narukawamasa <te2.icpo19@gmail.com>
2023-03-08 18:30:19 +0900

    二回目r-5-1

---
+++ JavaWcp/src/main/j_5/J_r_5_1.java
@@ -0,0 +1,20 @@
+package main.j_5;
+
+public class J_r_5_1 {
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+	
+	public static void introduceOneself() {
+		String name = "亀";
+		int age = 20;
+		double f = 12.0;
+		char a = '辰';
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(f);
+		System.out.println(a);
+	}
+
+}
