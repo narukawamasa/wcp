91adfbe76ee6c25a4b9b8e177609e5a52bcad989
narukawamasa <te2.icpo19@gmail.com>
2023-03-08 16:41:30 +0900

    二回目5-4

---
+++ JavaWcp/src/main/j_5/J_5_4.java
@@ -0,0 +1,14 @@
+package main.j_5;
+
+public class J_5_4 {
+	public static void main(String[] args) {
+		hello("湊");
+		hello("朝香");
+		hello("菅原");
+	}
+	
+	public static void hello(String name) {
+		System.out.println(name + "さん、こんにちは！");
+	}
+
+}
