1d2d9cc59048237825655a1fdbe11de737ecd2d6
narukawamasa <te2.icpo19@gmail.com>
2023-03-08 17:02:50 +0900

    二回目5-10

---
+++ JavaWcp/src/main/j_5/J_5_10.java
@@ -0,0 +1,22 @@
+package main.j_5;
+
+public class J_5_10 {
+	public static int add(int x, int y) {
+		return x + y;
+	}
+	
+	public static double add(double x, double y) {
+		return x + y;
+	}
+	
+	public static String add(String x, String y) {
+		return x + y;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(add(10,20));
+		System.out.println(add(3.5,2.7));
+		System.out.println(add("Hello","World"));
+	}
+
+}
