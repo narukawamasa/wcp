dc5737e71bc3c30797dc5d56e1dad5424d2d96d9
narukawamasa <te2.icpo19@gmail.com>
2023-03-08 18:47:48 +0900

    二回目r-5-2

---
+++ JavaWcp/src/main/j_5/J_r_5_2.java
@@ -0,0 +1,17 @@
+package main.j_5;
+
+public class J_r_5_2 {
+	public static void main(String[] args) {
+		email();
+	}
+	
+	public static void email() {
+		String title = "aaa";
+		String address = "a@a";
+		String text = "Hello";
+		System.out.println(title + "に、以下のメールを送信しました");
+		System.out.println("件名:" + address);
+		System.out.println("本文:" + text);
+	}
+
+}
