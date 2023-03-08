d15211f23b29c7625f004992b2c0c165f4246ca8
narukawamasa <te2.icpo19@gmail.com>
2023-03-08 17:46:52 +0900

    二回目5-13

---
+++ JavaWcp/src/main/j_5/J_5_13.java
@@ -0,0 +1,19 @@
+package main.j_5;
+
+
+public class J_5_13 {
+	public static void incArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i]++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {1,2,3};
+		incArray(array);
+		for (int i : array) {
+			System.out.println(i);
+		}
+	}
+
+}
