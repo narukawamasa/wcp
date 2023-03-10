0e54bb9cf90063e0bc78cb0d3fb7a17aa699d71f
narukawamasa <te2.icpo19@gmail.com>
2023-03-10 19:03:22 +0900

    二回目r-6-1

---
+++ JavaWcp/src/main/J_6/J_r_6_1.java
@@ -0,0 +1,11 @@
+package main.J_6;
+
+public class J_r_6_1 {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		main.J_6.Kouhan.callDeae();
+		main.J_6.Kouhan.showMondokoro();
+	}
+
+}
---
+++ JavaWcp/src/main/J_6/Zenhan.java
@@ -0,0 +1,12 @@
+package main.J_6;
+
+public class Zenhan {
+	public static void doWarusa() {
+		System.out.println("きなこでござる。食えませんがの");
+	}
+	
+	public static void doTogame() {
+		System.out.println("このおいぼれの目はごまかせませんぞ。");
+	}
+
+}
