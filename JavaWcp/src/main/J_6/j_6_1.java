999e5f7ec787cdaea7436225f03ef4fdd615dff5
narukawamasa <te2.icpo19@gmail.com>
2023-03-10 18:09:57 +0900

    二回目6-4

--- JavaWcp/src/main/J_6/j_6_1.java
+++ JavaWcp/src/main/J_6/j_6_1.java
@@ -3,8 +3,8 @@ package main.J_6;
 public class j_6_1 {
 	public static void main(String[] args) {
 		int a = 10; int b = 2;
-		int total = tasu(a,b);
-		int delta = hiku(a,b);
+		int total = CalcLogic.tasu(a,b);
+		int delta = CalcLogic.hiku(a,b);
 		System.out.println("足すと" + total + "引くと" + delta);
 	}
 
