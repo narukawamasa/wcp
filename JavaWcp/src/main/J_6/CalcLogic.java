4d436983c80ca260cd73db7c22def96678bf61fc
narukawamasa <te2.icpo19@gmail.com>
2023-03-10 18:07:42 +0900

    二回目6-2

---
+++ JavaWcp/src/main/J_6/CalcLogic.java
@@ -0,0 +1,12 @@
+package main.J_6;
+
+public class CalcLogic {
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+	
+	public static int hiku(int a, int b) {
+		return (a - b);
+	}
+
+}
