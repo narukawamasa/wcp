45a3db657d153a0bcfbd1220651ba48437c0d308
narukawamasa <te2.icpo19@gmail.com>
2023-03-10 18:35:34 +0900

    二回目6-10

---
+++ JavaWcp/src/main/J_6/J_6_10.java
@@ -0,0 +1,13 @@
+package main.J_6;
+import java.util.Arrays;
+
+public class J_6_10 {
+	public static void main(String[] args) {
+		int[] heights = {172,149,152,191,155};
+		Arrays.sort(heights);
+		for (int h : heights) {
+			System.out.println(h);
+		}
+	}
+
+}
