dc3b1c05f21acdc66950db383604aba11de273a4
narukawamasa <te2.icpo19@gmail.com>
2023-03-11 15:59:41 +0900

    二回目8-12

--- JavaWcp/src/main/j_8/Main.java
+++ JavaWcp/src/main/j_8/Main.java
@@ -2,7 +2,10 @@ package main.j_8;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "を生み出しました!");
 	}
 
 }
