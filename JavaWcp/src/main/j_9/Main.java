c4a88a6cfdc3cce531c2b002cc1876d24e21da50
narukawamasa <te2.icpo19@gmail.com>
2023-03-13 19:10:17 +0900

    二回目9-6

--- JavaWcp/src/main/j_9/Main.java
+++ JavaWcp/src/main/j_9/Main.java
@@ -5,11 +5,21 @@ public class Main {
 		Sword s = new Sword();
 		s.name = "炎の剣";
 		s.damage = 10;
-		Hero h = new Hero();
-		h.name = "ミナト";
-		h.hp = 100;
-		h.sword = s;
-		System.out.println("現在の武器は" + h.sword.name);
+		Hero h1 = new Hero();
+		h1.name = "ミナト";
+		h1.hp = 100;
+		h1.sword = s;
+		Hero h2 = new Hero();
+		h2.name = "アサカ";
+		h2.hp = 100;
+		h2.sword = s;
+		Wizard w = new Wizard();
+		w.name = "スガワラ";
+		w.hp = 50;
+		w.heal(h1);
+		w.heal(h2);
+		w.heal(h2);
+		System.out.println("現在の武器は" + h1.sword.name);
 	}
 
 }
