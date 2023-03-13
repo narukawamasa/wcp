faabdf373d061465ae9f173e05510827e952e160
narukawamasa <te2.icpo19@gmail.com>
2023-03-13 19:14:27 +0900

    二回目9-8

--- JavaWcp/src/main/j_9/Hero.java
+++ JavaWcp/src/main/j_9/Hero.java
@@ -5,6 +5,10 @@ public class Hero {
 	int hp;
 	Sword sword;
 	
+	public Hero() {
+		this.hp = 100;
+	}
+	
 	public void attack() {
 		System.out.println(this.name + "は攻撃した！");
 		System.out.println("敵に５ポイントのダメージをあたえた！");
