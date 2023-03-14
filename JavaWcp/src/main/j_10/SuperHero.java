84a32a85473b8f67862887265699625929c60ddb
narukawamasa <te2.icpo19@gmail.com>
2023-03-14 18:16:51 +0900

    二回目10-3

--- JavaWcp/src/main/j_10/SuperHero.java
+++ JavaWcp/src/main/j_10/SuperHero.java
@@ -1,19 +1,7 @@
 package main.j_10;
 
-public class SuperHero {
-	String name = "ミナト";
-	int hp = 100;
+public class SuperHero extends Hero {
 	boolean flying;
-	
-	public void attack(Matango m) {
-		System.out.println(this.name + "の攻撃");
-		m.hp -= 5;
-		System.out.println("5ポイントのダメージをあたえた！");
-	}
-	
-	public void run() {
-		System.out.println(this.name + "は逃げ出した！");
-	}
 	
 	public void fly() {
 		this.flying = true;
