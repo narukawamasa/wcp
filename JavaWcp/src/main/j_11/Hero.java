c86540bdf5afc1b8f8b36230fb59168887cca33e
narukawamasa <te2.icpo19@gmail.com>
2023-03-15 17:24:34 +0900

    二回目11-6

--- JavaWcp/src/main/j_11/Hero.java
+++ JavaWcp/src/main/j_11/Hero.java
@@ -4,7 +4,7 @@ public class Hero extends Character {
 	String name = "ミナト";
 	int hp = 100;
 	
-	public void atack(Matango m) {
+	public void attack(Matango m) {
 		System.out.println(this.name + "の攻撃");
 		m.hp -= 10;
 		System.out.println("10ポイントのダメージをあたえた！");
--- JavaWcp/src/main/j_11/Main.java
+++ JavaWcp/src/main/j_11/Main.java
@@ -2,6 +2,7 @@ package main.j_11;
 
 public class Main {
 	public static void main(String[] args) {
+		Character c = new Character();
 		Hero h = new Hero();
 		Matango m = new Matango('A');
 		h.attack(m);
