c6cbc2c136830fe6ebcc315cae2410244f3032aa
narukawamasa <te2.icpo19@gmail.com>
2023-03-15 17:16:34 +0900

    二回目11-1

---
+++ JavaWcp/src/main/j_11/Character.java
@@ -0,0 +1,17 @@
+package main.j_11;
+
+public class Character {
+	String name;
+	int hp;
+	
+	public void run() {
+		System.out.println(this.name + "は逃げ出した！");
+	}
+	
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃！");
+		m.hp -= ??;
+		System.out.println("敵に？？ポイントのダメージをあたえた！");
+	}
+
+}
