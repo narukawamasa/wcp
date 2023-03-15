528e338445e4fde2a65a51cbd0e0e9092dd16451
narukawamasa <te2.icpo19@gmail.com>
2023-03-15 17:22:21 +0900

    二回目11-4

--- JavaWcp/src/main/j_11/Hero.java
+++ JavaWcp/src/main/j_11/Hero.java
@@ -4,11 +4,11 @@ public class Hero extends Character {
 	String name = "ミナト";
 	int hp = 100;
 	
-	public void attack(Matango m) {
-		System.out.println(this.name + "の攻撃");
-		m.hp -= 10;
-		System.out.println("10ポイントのダメージをあたえた！");
-	}
+//	public void attack(Matango m) {
+//		System.out.println(this.name + "の攻撃");
+//		m.hp -= 10;
+//		System.out.println("10ポイントのダメージをあたえた！");
+//	}
 	public Hero() {
 		System.out.println("Heroのコンストラクタが動作");
 	}
