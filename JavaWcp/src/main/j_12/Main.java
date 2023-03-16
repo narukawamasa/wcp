0ef4bd43c5742257ac89511025c78b2f2cc5e70b
narukawamasa <te2.icpo19@gmail.com>
2023-03-16 16:31:44 +0900

    二回目12-6

--- JavaWcp/src/main/j_12/Main.java
+++ JavaWcp/src/main/j_12/Main.java
@@ -2,10 +2,17 @@ package main.j_12;
 
 public class Main {
 	public static void main(String[] args) {
-		Slime s = new Slime();
-		Monster m = new Slime();
-		s.run();
-		m.run();
+		Hero h1 = new Hero();
+		Hero h2 = new Hero();
+		Thief t1 = new Thief("a");
+		Wizard w1 = new Wizard();
+		Wizard w2 = new Wizard();
+		
+		h1.hp += 50;
+		h2.hp += 50;
+		t1.hp += 50;
+		w1.hp += 50;
+		w2.hp += 50;
 	}
 
 }
