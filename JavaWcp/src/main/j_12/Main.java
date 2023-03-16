075b6508b9da57b30d05e5464ee9143bb9db663c
narukawamasa <te2.icpo19@gmail.com>
2023-03-16 16:35:39 +0900

    二回目12-7

--- JavaWcp/src/main/j_12/Main.java
+++ JavaWcp/src/main/j_12/Main.java
@@ -2,17 +2,16 @@ package main.j_12;
 
 public class Main {
 	public static void main(String[] args) {
-		Hero h1 = new Hero();
-		Hero h2 = new Hero();
-		Thief t1 = new Thief("a");
-		Wizard w1 = new Wizard();
-		Wizard w2 = new Wizard();
+		Character[] c = new Character[5];
+		c[0] = new Hero();
+		c[1] = new Hero();
+		c[2] = new Thief("a");
+		c[3] = new Wizard();
+		c[4] = new Wizard();
 		
-		h1.hp += 50;
-		h2.hp += 50;
-		t1.hp += 50;
-		w1.hp += 50;
-		w2.hp += 50;
+		for (Character ch : c) {
+			ch.hp += 50;
+		}
 	}
 
 }
