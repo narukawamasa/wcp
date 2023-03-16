0906df20249372acbc4569d0506b6229ffd99084
narukawamasa <te2.icpo19@gmail.com>
2023-03-16 16:56:51 +0900

    二回目r-12-3

--- JavaWcp/src/main/j_12/Main.java
+++ JavaWcp/src/main/j_12/Main.java
@@ -2,13 +2,14 @@ package main.j_12;
 
 public class Main {
 	public static void main(String[] args) {
-		Monster[] monsters = new Monster[3];
-		monster[0] = new Slime();
-		monster[1] = new Goblin();
-		monster[2] = new DeathBat();
-		for (Monster m : monsters) {
-			m.run();
+		Y[] y = new Y[2];
+		y[0] = new A();
+		y[1] = new B();
+		
+		for (Y yy : y) {
+			yy.b();
 		}
+		
 	}
 
 }
