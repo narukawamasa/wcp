1af30693b19c455e6b9953ede4f54bd067b8c342
narukawamasa <te2.icpo19@gmail.com>
2023-03-16 16:17:16 +0900

    二回目12-4

--- JavaWcp/src/main/j_12/Main.java
+++ JavaWcp/src/main/j_12/Main.java
@@ -3,10 +3,11 @@ package main.j_12;
 public class Main {
 	public static void main(String[] args) {
 		Wizard w = new Wizard();
+		Character c = w;
 		Matango m = new Matango('A');
-		w.name = "アサカ";
-		w.attack(m);
-		w.fireball(m);
+		c.name = "アサカ";
+		c.attack(m);
+		c.fireball(m);
 	}
 
 }
