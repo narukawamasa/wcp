9a018276f5710cefeb5e9a433cac4ae88544812f
narukawamasa <te2.icpo19@gmail.com>
2023-03-21 16:53:49 +0900

    二回目r-13-4

--- JavaWcp/src/main/j_13/Wand.java
+++ JavaWcp/src/main/j_13/Wand.java
@@ -11,9 +11,15 @@ public class Wand {
 		return this.power;
 	}
 	public void setName(String name) {
+		if (name == null || name.length() < 3) {
+			throw new IllegalArgumentException("名前が短い。処理を中断");
+		}
 		this.name = name;
 	}
 	public void setPower(double power) {
+		if (power < 0.5 || power > 100) {
+			throw new IllegalArgumentException("範囲外の数値。処理を中断");
+		}
 		this.power = power;
 	}
 
--- JavaWcp/src/main/j_13/Wizard.java
+++ JavaWcp/src/main/j_13/Wizard.java
@@ -13,12 +13,34 @@ public class Wizard {
 		System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
 	}
 	public int getHp() {return this.hp;}
-	public void setHp(int hp) {this.hp = hp;}
+	public void setHp(int hp) {
+		if (hp < 0) {
+			this.hp = 0;
+		} else {
+			this.hp = hp;
+		}
+		
+		}
 	public int getMp() {return this.mp;}
-	public void setMp(int mp) {this.mp = mp;}
+	public void setMp(int mp) {
+		if (mp < 0) {
+			throw new IllegalArgumentException("異常値です。処理を中断");
+		}
+		this.mp = mp;
+		}
 	public String getName() {return this.name;}
-	public void setName(String name) {this.name = name;}
+	public void setName(String name) {
+		if (name == null || name.length() < 3) {
+			throw new IllegalArgumentException("名前が短い。処理を中断");
+		}
+		this.name = name;
+		}
 	public Wand getWand() {return this.wand;}
-	public void setWand(Wand wand) { this.wand = wand;}
+	public void setWand(Wand wand) {
+		if (wand == null) {
+			throw new IllegalArgumentException("nullです。処理を中断");
+		}
+		this.wand = wand;
+		}
 
 }
