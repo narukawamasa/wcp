0fed95d648963d47ed34544d70100453ecff6d36
narukawamasa <te2.icpo19@gmail.com>
2023-03-23 18:04:15 +0900

    二回目r-14-2

---
+++ JavaWcp/src/main/j_14/Cleric.java
@@ -0,0 +1,25 @@
+package main.j_14;
+
+public class Cleric {
+	String name;
+	int hp = 50;
+	static final int MAX_HP = 50;
+	int mp = 10;
+	static final int MAX_MP = 10;
+	
+	public void selfAid() {
+		System.out.println(this.name + "は、セルフエイドを唱えた！");
+		this.mp -= 5;
+		this.hp = this.MAX_HP;
+		
+		System.out.println("HPが最大まで回復した");
+	}
+	
+//	public int pray(int sec) {
+//		int c = this.MAX_MP - this.mp;
+//		int recover = sec + new Random().nextInt(2);
+//		int recoverActual = Math.min(c, recover);
+//		this.mp += recoverActual;
+//	}
+
+}
