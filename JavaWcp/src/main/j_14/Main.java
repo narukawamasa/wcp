6c2efddf972bb56b1abbc6c8c73137daa489a0a4
narukawamasa <te2.icpo19@gmail.com>
2023-03-23 17:47:30 +0900

    二回目14-14

--- JavaWcp/src/main/j_14/Main.java
+++ JavaWcp/src/main/j_14/Main.java
@@ -2,22 +2,10 @@ package main.j_14;
 
 public class Main {
 	public static void main(String[] args) {
-//		Hero h1 = new Hero();
-//		h1.name = "ミナト";
-//		h1.hp = 100;
-//		Hero h2 = new Hero();
-//		h2.name = "ミナト";
-//		h2.hp = 100;
-		Hero.money = 100;
+		Hero.setRandomMoney();
 		System.out.println(Hero.money);
-//		System.out.println(h1.money);
-//		h1.money = 300;
-//		System.out.println(h2.money);
-//		if (h1.equals(h2) == true) {
-//			System.out.println("同じ内容です。");
-//		} else {
-//			System.out.println("違う内容です");
-//		}
+		Hero h1 = new Hero();
+		System.out.println(h1.money);
 	}
 
 }
