d05abafb37f68417a79f092e97b8a46a40841174
narukawamasa <te2.icpo19@gmail.com>
2023-03-23 16:50:48 +0900

    二回目14-2

--- JavaWcp/src/main/j_14/Main.java
+++ JavaWcp/src/main/j_14/Main.java
@@ -2,9 +2,9 @@ package main.j_14;
 
 public class Main {
 	public static void main(String[] args) {
-		Empty e = new Empty();
-		String s = e.toString();
-		System.out.println(s);
+		Object o1 = new Empty();
+		Object o2 = new Hero();
+		Object o3 =  "こんにちは";
 	}
 
 }
