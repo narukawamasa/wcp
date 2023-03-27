f248fd02ed188f1cec2cf76fea5635976fd264cb
narukawamasa <te2.icpo19@gmail.com>
2023-03-27 17:24:22 +0900

    二回目15-8

--- JavaWcp/src/main/j_15/Main.java
+++ JavaWcp/src/main/j_15/Main.java
@@ -3,10 +3,8 @@ package main.j_15;
 public class Main {
 	public static void main(String[] args) {
 		String s = "abc,def:ghi";
-		String[] words = s.split("[,:]");
-		for (String w : words) {
-			System.out.print(w + "->");
-		}
+		String w = s.replaceAll("[beh]","X");
+		System.out.println(w);
 	}
 
 }
