fcaddd3c011ba785795f38bc509c4f349d057166
narukawamasa <te2.icpo19@gmail.com>
2023-03-27 18:04:30 +0900

    二回目15-13

--- JavaWcp/src/main/j_15/Main.java
+++ JavaWcp/src/main/j_15/Main.java
@@ -1,19 +1,16 @@
 package main.j_15;
 
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
-	public static void main(String[] args) {
-		Calendar c = Calendar.getInstance();
-		c.set(2019, 8,22,1,23,45);
-		c.set(Calendar.MONTH, 9);
-		Date d = c.getTime();
+	public static void main(String[] args) throws Exception {
+		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date d = f.parse("2020/09/22 01:23:45");
 		System.out.println(d);
 		Date now = new Date();
-		c.setTime(d);
-		int y = c.get(Calendar.YEAR);
-		System.out.println("今年は" + y + "年です");
+		String s = f.format(now);
+		System.out.println("現在は" + s + "です");
 	}
 
 }
