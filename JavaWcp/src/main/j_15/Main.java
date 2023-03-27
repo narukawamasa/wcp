74b161d976c6996b36b265f6cf8ca8a0c699445b
narukawamasa <te2.icpo19@gmail.com>
2023-03-27 18:29:03 +0900

    二回目15-14

--- JavaWcp/src/main/j_15/Main.java
+++ JavaWcp/src/main/j_15/Main.java
@@ -1,16 +1,28 @@
 package main.j_15;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		Date d = f.parse("2020/09/22 01:23:45");
-		System.out.println(d);
-		Date now = new Date();
-		String s = f.format(now);
-		System.out.println("現在は" + s + "です");
+		Instant i1 = Instant.now();
+		
+		Instant i2 = Instant.ofEpochMilli(160070542582L);
+		long l = i2.toEpochMilli();
+		
+		ZonedDateTime z1 = ZonedDateTime.now();
+		ZonedDateTime z2 = ZonedDateTime.of(2020, 1,2,3,4,5,6,ZoneId.of("Asia/Tokyo"));
+		
+		Instant i3 = z2.toInstant();
+		ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
+		
+		System.out.println("東京:" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
+		System.out.println("ロンドン:" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
+		
+		if (z2.isEqual(z3)) {
+			System.out.println("これらは同じ習慣をさしています");
+		}
 	}
 
 }
