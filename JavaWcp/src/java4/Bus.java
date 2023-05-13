8e3ac19e2a0f3651a37bdf8ec9be41772b9121db
narukawamasa <te2.icpo19@gmail.com>
2023-05-13 17:25:48 +0900

    四章演習問題１

---
+++ JavaWcp/src/java4/Bus.java
@@ -0,0 +1,11 @@
+package java4;
+
+public class Bus extends Vehicle {
+	public Bus(int crewNum){
+	    this.crewNum = crewNum;
+	  }
+	public void showCrewNum(){
+	  System.out.println("バスの乗客：" + crewNum + "名");
+	}
+
+}
---
+++ JavaWcp/src/java4/Vehicle.java
@@ -0,0 +1,8 @@
+package java4;
+
+public abstract class  Vehicle {
+	protected int crewNum;
+	
+	public abstract void showCrewNum();
+
+}
